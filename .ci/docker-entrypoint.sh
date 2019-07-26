01f4fe934d0edb4c404a85c8a6d033a5a99be3cf
Igor Recio <igor.recio.h@gmail.com>
2019-07-27 00:03:38 +0200

    Reduced Travis verbosity

--- .ci/docker-entrypoint.sh
+++ .ci/docker-entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 cd /home/root || exit
-wget https://github.com/Kitware/CMake/releases/download/v3.15.1/cmake-3.15.1.tar.gz
-tar -zxvf cmake-3.15.1.tar.gz
+wget https://github.com/Kitware/CMake/releases/download/v3.15.1/cmake-3.15.1.tar.gz > /dev/null
+tar -zxvf cmake-3.15.1.tar.gz > /dev/null
 cd cmake-3.15.1
-./bootstrap
-make
-make install
+./bootstrap > /dev/null
+make > /dev/null
+make install > /dev/null
 cd ..
 mkdir build && cd build
 cmake .. && make
