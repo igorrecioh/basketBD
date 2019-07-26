f7a1cd512c3cfb2f11c4f1a3f02f541aff09724d
Igor Recio <igor.recio.h@gmail.com>
2019-07-26 23:44:22 +0200

    Fixing cmake issues

--- .ci/docker-entrypoint.sh
+++ .ci/docker-entrypoint.sh
@@ -2,8 +2,8 @@
 
 cd /home/root || exit
 wget https://github.com/Kitware/CMake/releases/download/v3.15.1/cmake-3.15.1.tar.gz
-tar -zxvf cmake-3.15.1-Linux-x86_64.tar.gz
-cd cmake-3.15.1-Linux-x86_64
+tar -zxvf cmake-3.15.1.tar.gz
+cd cmake-3.15.1
 ./bootstrap
 make
 make install
